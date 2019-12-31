c961b3ebc7d0fcbdb4466640d4c33d8968d7a21e
Steinkellner, Kai <Kai.Steinkellner@yahoo.de>
2019-12-31 15:32:11 +0100

    Updates

---
+++ precache-manifest.ce2b4d6e63186a95bbb51cb44e9e9c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361982cd7763f968c6f38a70960c082f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b1c30a04d30f486ba6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1097619c3b23d11f3ba9",
+    "url": "/static/js/2.3a76e6fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3a76e6fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06b1c30a04d30f486ba6",
+    "url": "/static/js/main.f1238802.chunk.js"
+  },
+  {
+    "revision": "de7bb4f6349ac119bbfd",
+    "url": "/static/js/runtime-main.1f724eee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
